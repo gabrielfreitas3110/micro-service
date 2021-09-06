847ffa9fd4b8736a56c926443002c5f7ca468218
Gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-06 17:19:55 -0230

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker(name, daily_Income) VALUES ('Gabriel', 200.0);
+INSERT INTO tb_worker(name, daily_Income) VALUES ('Maria', 300.0);
+INSERT INTO tb_worker(name, daily_Income) VALUES ('Washington', 250.0);
