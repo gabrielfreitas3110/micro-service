bd343ac77b9f8051cbb8748fb4bae3afd2d3d2bc
Gabriel Augusto <gabriel.freitas3110@gmail.com>
2021-09-09 22:10:39 -0300

    Project hr-api-gateway-springcloud created

---
+++ hr-api-gateway-springcloud/src/main/java/com/example/hrapigatewayspringcloud/HrApiGatewaySpringcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrapigatewayspringcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewaySpringcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewaySpringcloudApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-springcloud/src/test/java/com/example/hrapigatewayspringcloud/HrApiGatewaySpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrapigatewayspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewaySpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
