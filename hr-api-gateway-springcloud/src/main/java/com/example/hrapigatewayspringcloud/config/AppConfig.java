8218ca18b6358b0849d5148d635b8c9088800e96
gabrielfreitas3110 <gabriel.freitas3110@gmail.com>
2021-09-27 02:01:50 -0300

    Springcloud authorization

--- hr-api-gateway-springcloud/src/main/java/com/example/hrapigatewayspringcloud/config/AppConfig.java
+++ hr-api-gateway-springcloud/src/main/java/com/example/hrapigatewayspringcloud/config/AppConfig.java
@@ -1,8 +1,7 @@
-package com.example.hroauth.config;
+package com.example.hrapigatewayspringcloud.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
@@ -10,17 +9,12 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 public class AppConfig {
 
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-	
-	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
 		tokenConverter.setSigningKey("MY-SECRET-KEY");
 		return tokenConverter;
 	}
-	
+
 	@Bean
 	public JwtTokenStore tokenStore() {
 		return new JwtTokenStore(accessTokenConverter());
